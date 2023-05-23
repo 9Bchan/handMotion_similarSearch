e9c63afff38b913735613039d661773ee13715e4
9Bchan <onct.honda@gmail.com>
2023-05-23 18:44:20 +0900

    データの読み込み部分作成完了

--- workspace/ModifiedProject/myfunc.py
+++ workspace/ModifiedProject/myfunc.py
@@ -10,7 +10,7 @@ def printline(contents):
     print("{:<13} : {:<4} : ".format(frame_name[:13], frame_line), end="")
     print(contents)
 
-def printlist(contents):
+def printlines(contents):
     frame = inspect.currentframe().f_back
     frame_name = os.path.basename(frame.f_code.co_filename)
     if len(frame_name) > 10:
@@ -18,3 +18,13 @@ def printlist(contents):
     frame_line = frame.f_lineno
     print("{:<13} : {:<4} : ".format(frame_name[:13], frame_line))
     print(contents)
+
+def printlist(contents):
+    frame = inspect.currentframe().f_back
+    frame_name = os.path.basename(frame.f_code.co_filename)
+    if len(frame_name) > 10:
+        frame_name = frame_name[:10] + "..."
+    frame_line = frame.f_lineno
+    print("{:<13} : {:<4} : ".format(frame_name[:13], frame_line))
+    for index in contents:
+        print(index)
